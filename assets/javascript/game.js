d986364fb879968af88b55344075fd3c8b92dcb6
Theo <theobasallaje@gmail.com>
2019-01-18 20:51:56 -0600

    added event listener to gifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
-// Initial array of movies
-var gifs = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
+// Initial array of gif
+var gifs = ["The Matrix", "cats", "birds", "The Lion King"];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
+// displayGif function re-renders the HTML to display the appropriate content
 function displayGif() {
 
     var gif = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=TEBjSdTv3FNMqy7oIiSsHjkTI7xIRDby"; 
+    var queryURL = "http://api.giphy.com/v1/gifs/search?limit=10&q="+ gif +"&api_key=TEBjSdTv3FNMqy7oIiSsHjkTI7xIRDby"; 
 
-    // Creates AJAX call for the specific movie button being clicked
+    // Creates AJAX call for the specific gif button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -18,29 +18,36 @@ function displayGif() {
         var gifDiv = $("<div class='gif'>");
         // Retrieves the Rating Data
         for (var i=0; i<response.data.length; i++){
-        var responseGif = response.data[i].images.downsized_medium.url;
+        var responseGif = response.data[i].images.fixed_height_small_still.url;
+        var responseGifAnimate = response.data[i].images.fixed_height_small.url;
         console.log(responseGif);
-        var gifElement = $("<img src=" + responseGif + ">");
+        console.log(responseGifAnimate);
+        var gifElement = $(`<img class="response" 
+                            src="${responseGif}" 
+                            data-still="${responseGif}" 
+                            data-animate="${responseGifAnimate}"
+                            data-state="still">`);
         // Appends the image
         gifDiv.append(gifElement);
         }
+        $("#gif-view").empty();
         $("#gif-view").prepend(gifDiv);
     });
 }
 
-// Function for displaying movie data
+// Function for displaying gif data
 function renderButtons() {
 
-    // Deletes the movies prior to adding new movies
+    // Deletes the gifs prior to adding new movies
     // (this is necessary otherwise you will have repeat buttons)
     $("#buttons-view").empty();
-    // Loops through the array of movies
+    // Loops through the array of gifs
     for (var i = 0; i < gifs.length; i++) {
 
         // Then dynamicaly generates buttons for each movie in the array
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
-        // Adds a class of movie to our button
+        // Adds a class of gif to our button
         a.addClass("gif");
         // Added a data-attribute
         a.attr("data-name", gifs[i]);
@@ -51,21 +58,36 @@ function renderButtons() {
     }
 }
 
-// This function handles events where the add movie button is clicked
+// This function handles events where the add gif button is clicked
 $("#add-gif").on("click", function (event) {
     event.preventDefault();
     // This line of code will grab the input from the textbox
     var gif = $("#gif-input").val().trim();
 
-    // The movie from the textbox is then added to our array
+    // The gif from the textbox is then added to our array
     gifs.push(gif);
 
-    // Calling renderButtons which handles the processing of our movie array
+    // Calling renderButtons which handles the processing of our gif array
     renderButtons();
 });
 
-// Adding click event listeners to all elements with a class of "movie"
+// Adding click event listeners to all elements with a class of "gif"
 $(document).on("click", ".gif", displayGif);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+
+$(".response").on("click", function(){
+    console.log(this);
+    var state = $(this).attr("data-state");
+
+    if (state === "still"){
+        console.log("in if");
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        console.log("in else");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
