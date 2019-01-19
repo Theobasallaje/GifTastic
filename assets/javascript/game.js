6f3d96754106c1b0ea0eeed10caca4ba48c43648
Theo <theobasallaje@gmail.com>
2019-01-18 19:55:25 -0600

    complete v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,71 @@
+// Initial array of movies
+var gifs = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
 
-var questions = [
-    {
-        question: "Question 1?",
-        options: ["answer", "O2", "O3", "O4"],
-        answer: "answer"
-    },
-    {
-        question: "Question 2?",
-        options: ["answer", "O2", "O3", "O4"],
-        answer: "answer"
-    },
-    {
-        question: "Question 3?",
-        options: ["answer", "O2", "O3", "O4"],
-        answer: "answer"
-    }
-];
-//objects
-var results = {
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayGif() {
 
-}
+    var gif = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=TEBjSdTv3FNMqy7oIiSsHjkTI7xIRDby"; 
 
-//global variables
-var questionsElement = $("#questions");
-var submit = $("#submit");
-var timerElement = $("#timer");
-var timer = 60
-var timerInterval;
-
-var correct = 0;
-var wrong = 0;
-var counter = 0;
-
-//functions
-function startGame(){
-    correct = 0;
-    wrong=0;
-    timer=60;
-    timerInterval = setInterval(function(){ 
-        if(timer > 1){ 
-            timer-- 
-            timerElement.html("Timer: " + timer);
-        } else{ 
-            alert("time out")
-            clearInterval(timerInterval)
-        } 
-    }
-    
-    , 1000)
-    timerElement.html("Timer: " + timer);
-    questionsElement.empty();
-    displayQuestions();
-}
-
-startGame()
-
-
-// function displayQuestions(){
-//     for (var i=0; i<questions.length; i++){
-//         questionsElement.append(`<div class="card">${questions[i].question}</div>`);
-//         for (var j=0; j< questions[i].options.length; j++){
-//             questionsElement.append(`<input number=${i+1} data-value="${questions[i].options[j]}" name=${questions[i].question} type="radio" class="options">${questions[i].options[j]}`);
-//         }
-//     }
-// }
-
-function displayQuestion(counter){
-    
-    counter++;
-}
-
-
-$(document).on('click', '.options', function(){
-    var value = $(this).attr('data-value');
-    var questionNumber = $(this).attr('number');
-    // alert(questionNumber + value);
-    results[questionNumber] = value;
-    console.log(results);
-})
-
-submit.on('click', function(){
-    for (var i=0; i<questions.length; i++){
-        if (questions[i].answer === results[i+1]){
-            correct++;
+    // Creates AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        
+        // Creates a div to hold the movie
+        var gifDiv = $("<div class='gif'>");
+        // Retrieves the Rating Data
+        for (var i=0; i<response.data.length; i++){
+        var responseGif = response.data[i].images.downsized_medium.url;
+        console.log(responseGif);
+        var gifElement = $("<img src=" + responseGif + ">");
+        // Appends the image
+        gifDiv.append(gifElement);
         }
-        else {
-            wrong++;
-        }
+        $("#gif-view").prepend(gifDiv);
+    });
+}
+
+// Function for displaying movie data
+function renderButtons() {
+
+    // Deletes the movies prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+    // Loops through the array of movies
+    for (var i = 0; i < gifs.length; i++) {
+
+        // Then dynamicaly generates buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adds a class of movie to our button
+        a.addClass("gif");
+        // Added a data-attribute
+        a.attr("data-name", gifs[i]);
+        // Provided the initial button text
+        a.text(gifs[i]);
+        // Added the button to the buttons-view div
+        $("#buttons-view").append(a);
     }
-    clearInterval(timerInterval);
-    console.log(correct);
-    console.log(wrong);
-    $('#questions').html(`<div> you had this many correct :  ${correct}</div>
-                        <div> you had this many wrong : ${wrong}</div>`);
-    setTimeout(startGame, 2000)
-})
+}
 
+// This function handles events where the add movie button is clicked
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var gif = $("#gif-input").val().trim();
 
+    // The movie from the textbox is then added to our array
+    gifs.push(gif);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+// Adding click event listeners to all elements with a class of "movie"
+$(document).on("click", ".gif", displayGif);
+
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
